230c02003afa421abe5868685ce401710b9afb4d
NabilDawud <nabeeldawud@gmail.com>
2026-07-30 19:28:08 +0300

    feat(dashboard): configure CMS dashboard view

---
+++ resources/views/cms/temp.blade.php
@@ -0,0 +1,16 @@
+@extends('cms.parent');
+
+@section('title', 'Test')
+
+@section('main-title', 'Main Title')
+
+@section('sub-title', 'Sub Title')
+
+@section('styles')
+@endsection
+
+@section('content')
+@endsection
+
+@section('scripts')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('cms/admin/')->group(function () {
+    Route::view('/parent', 'cms.parent');
+    Route::view('/temp', 'cms.temp');
+});
